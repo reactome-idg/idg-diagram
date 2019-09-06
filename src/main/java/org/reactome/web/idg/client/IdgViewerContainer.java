0ee7f1e5f3af839760c105606d93c2a1779121c2
Tim Brunson <tjbrunson@gmail.com>
2019-09-06 13:29:28 -0700

    made container that extends base diagram container

---
+++ src/main/java/org/reactome/web/idg/client/IdgViewerContainer.java
@@ -0,0 +1,13 @@
+package org.reactome.web.idg.client;
+
+import org.reactome.web.diagram.client.ViewerContainer;
+
+import com.google.gwt.event.shared.EventBus;
+
+public class IdgViewerContainer extends ViewerContainer{
+
+	public IdgViewerContainer(EventBus eventBus) {
+		super(eventBus);
+	}
+
+}
