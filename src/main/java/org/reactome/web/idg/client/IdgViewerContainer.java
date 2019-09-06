8d5ef8b5a6645a4985639a7752c42e14606a6bb8
Tim Brunson <tjbrunson@gmail.com>
2019-09-06 14:31:05 -0700

    initial setup

--- src/main/java/org/reactome/web/idg/client/IdgViewerContainer.java
+++ src/main/java/org/reactome/web/idg/client/IdgViewerContainer.java
@@ -1,13 +1,29 @@
 package org.reactome.web.idg.client;
 
 import org.reactome.web.diagram.client.ViewerContainer;
+import org.reactome.web.diagram.common.IconButton;
+import org.reactome.web.idg.client.visualisers.fiview.FIViewVisualiser;
 
 import com.google.gwt.event.shared.EventBus;
 
 public class IdgViewerContainer extends ViewerContainer{
 
+	EventBus eventBus;
+	
+	IconButton fiviewButton;
+	
 	public IdgViewerContainer(EventBus eventBus) {
 		super(eventBus);
+		this.eventBus = eventBus;
+		
+		initialise();
 	}
 
+	@Override
+	protected void initialise() {
+		super.initialise();
+		this.add(new FIViewVisualiser(eventBus));
+		
+	}
+	
 }
