f0699893944e5c28f748eb66645c3b1db44d9099
Tim Brunson <tjbrunson@gmail.com>
2019-09-06 13:27:16 -0700

    updated entry point

--- src/main/java/org/reactome/web/test/WidgetTest.java
+++ src/main/java/org/reactome/web/test/WidgetTest.java
@@ -3,14 +3,12 @@ package org.reactome.web.test;
 import org.reactome.web.idg.client.IdgDiagramViewer;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.RootPanel;
 
 
 public class WidgetTest implements EntryPoint{
 
 	public void onModuleLoad() {
-		Window.alert("GOT HERE");
 		IdgDiagramViewer view = new IdgDiagramViewer();
 		RootPanel.get().add(view);
 	}
