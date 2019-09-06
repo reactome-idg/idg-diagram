14323083b0be348896af3ee22e5370918749bd63
Tim Brunson <tjbrunson@gmail.com>
2019-09-06 13:28:43 -0700

    simplified test view

--- src/main/java/org/reactome/web/idg/client/IdgDiagramViewer.java
+++ src/main/java/org/reactome/web/idg/client/IdgDiagramViewer.java
@@ -1,8 +1,5 @@
 package org.reactome.web.idg.client;
 
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Label;
@@ -12,7 +9,6 @@ import com.google.gwt.user.client.ui.Widget;
 
 public class IdgDiagramViewer extends Composite implements IsWidget {
 
-	private Button test;
 	
 	public IdgDiagramViewer() {
 		
@@ -23,14 +19,12 @@ public class IdgDiagramViewer extends Composite implements IsWidget {
 	private void initialise() {
 		SimplePanel view = new SimplePanel();
 		view.getElement().setId("test div");
-		test = new Button("test");
-		view.add(test);
 		view.add(new Label("Hello world"));
 		initWidget(view);	}
  
 	@Override
 	public Widget asWidget() {
-		return this;
+		return null;
 	}
 	
 }
