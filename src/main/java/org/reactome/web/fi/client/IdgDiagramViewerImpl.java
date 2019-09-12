b4c0b6b7fe228db025507003afa7fe535eb5216b
Tim Brunson <tjbrunson@gmail.com>
2019-09-12 11:14:51 -0700

    minor changes

--- src/main/java/org/reactome/web/fi/client/IdgDiagramViewerImpl.java
+++ src/main/java/org/reactome/web/fi/client/IdgDiagramViewerImpl.java
@@ -2,11 +2,14 @@ package org.reactome.web.fi.client;
 
 import org.reactome.web.diagram.client.DiagramViewerImpl;
 import org.reactome.web.diagram.client.ViewerContainer;
+import org.reactome.web.diagram.events.ContentLoadedEvent;
+import org.reactome.web.diagram.handlers.ContentLoadedHandler;
 
-public class IdgDiagramViewerImpl extends DiagramViewerImpl {
+public class IdgDiagramViewerImpl extends DiagramViewerImpl{
 	
 	public IdgDiagramViewerImpl() {
 		super();
+		
 	}
 	
 	@Override
@@ -14,6 +17,4 @@ public class IdgDiagramViewerImpl extends DiagramViewerImpl {
 		return new IdgViewerContainer(eventBus);
 		
 	}
-
-	
 }
