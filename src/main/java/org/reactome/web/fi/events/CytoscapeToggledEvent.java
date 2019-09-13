c0a703e683e234a279d374c0ea1cfabbc3c6b9e7
Tim Brunson <tjbrunson@gmail.com>
2019-09-13 11:26:31 -0700

    accepts context as an arguement now

--- src/main/java/org/reactome/web/fi/events/CytoscapeToggledEvent.java
+++ src/main/java/org/reactome/web/fi/events/CytoscapeToggledEvent.java
@@ -1,5 +1,6 @@
 package org.reactome.web.fi.events;
 
+import org.reactome.web.diagram.data.Context;
 import org.reactome.web.fi.client.flag.CytoscapeViewFlag;
 import org.reactome.web.fi.handlers.CytoscapeToggledHandler;
 
@@ -8,7 +9,12 @@ import com.google.gwt.event.shared.GwtEvent;
 public class CytoscapeToggledEvent extends GwtEvent<CytoscapeToggledHandler>{
     public static Type<CytoscapeToggledHandler> TYPE = new Type<>();
 
-
+    private Context context;
+    
+    public CytoscapeToggledEvent(Context context) {
+    	this.context = context;
+    }
+    
 	@Override
 	public Type<CytoscapeToggledHandler> getAssociatedType() {
 		return TYPE;
@@ -19,6 +25,10 @@ public class CytoscapeToggledEvent extends GwtEvent<CytoscapeToggledHandler>{
 		handler.onCytoscapeToggled(this);
 	}
 
+	public Context getContext() {
+		return this.context;
+	}
+	
 	@Override
 	public String toString() {
 		return "Cytoscape toggled to: " + CytoscapeViewFlag.isCytoscapeViewFlag();
