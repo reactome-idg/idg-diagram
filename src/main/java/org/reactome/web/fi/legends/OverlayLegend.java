9594ab48e19668d52b58ae24089f7e6eedb52f88
Tim Brunson <tjbrunson@gmail.com>
2019-10-28 10:41:32 -0700

    added label and close button to legend

--- src/main/java/org/reactome/web/fi/legends/OverlayLegend.java
+++ src/main/java/org/reactome/web/fi/legends/OverlayLegend.java
@@ -1,48 +1,60 @@
 package org.reactome.web.fi.legends;
 
+import org.reactome.web.diagram.common.PwpButton;
 import org.reactome.web.diagram.legends.LegendPanel;
-import org.reactome.web.diagram.profiles.analysis.AnalysisColours;
+import org.reactome.web.fi.events.OverlayDataLoadedEvent;
+import org.reactome.web.fi.events.OverlayDataResetEvent;
+import org.reactome.web.fi.handlers.OverlayDataLoadedHandler;
 
-import com.google.gwt.canvas.client.Canvas;
-import com.google.gwt.canvas.dom.client.CanvasGradient;
-import com.google.gwt.canvas.dom.client.Context2d;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.user.client.ui.InlineLabel;
 
-public class OverlayLegend extends LegendPanel{
+public class OverlayLegend extends LegendPanel implements ClickHandler,
+OverlayDataLoadedHandler{
 	
-	Canvas gradient;
+	InlineLabel message;
+	private PwpButton closeBtn;
 
 	public OverlayLegend(EventBus eventBus) {
 		super(eventBus);
-
-		this.gradient = createCanvas(30,200);
-		this.fillGradient();
 		
-		addStyleName(RESOURCES.getCSS().enrichmentLegend());
-		this.setVisible(true);
+		LegendPanelCSS css = RESOURCES.getCSS();
+
+		this.message = new InlineLabel();
+		this.message.setText("Overlay Data Types:");
+		this.add(this.message);
+		
+		this.closeBtn = new PwpButton("Close", css.close(), this);
+		this.add(this.closeBtn);
+		
+		initHandlers();
+		
+		addStyleName(RESOURCES.getCSS().enrichmentControl());
+		this.setHeight("63px");
+		this.setVisible(false);
 		
 	}
 
-	private Canvas createCanvas(int width, int height) {
-        Canvas canvas = Canvas.createIfSupported();
-        canvas.setCoordinateSpaceWidth(width);
-        canvas.setCoordinateSpaceHeight(height);
-        canvas.setPixelSize(width, height);
-        return canvas;
-    }
-	
-	private void fillGradient(){
-        Context2d ctx = this.gradient.getContext2d();
-        CanvasGradient grd = ctx.createLinearGradient(0, 0, 30, 200);
-        grd.addColorStop(0, AnalysisColours.get().PROFILE.getEnrichment().getGradient().getMin());
-        grd.addColorStop(1, AnalysisColours.get().PROFILE.getEnrichment().getGradient().getMax());
+	private void initHandlers() {
+		eventBus.addHandler(OverlayDataLoadedEvent.TYPE, this);
+	}
 
-        ctx.clearRect(0, 0, this.gradient.getCoordinateSpaceWidth(), this.gradient.getCoordinateSpaceHeight());
-        ctx.setFillStyle(grd);
-        ctx.beginPath();
-        ctx.fillRect(0, 0, 30, 200);
-        ctx.closePath();
-    }
+	@Override
+	public void onClick(ClickEvent event) {
+		if(event.getSource().equals(this.closeBtn)) {
+			this.setVisible(false);
+			eventBus.fireEventFromSource(new OverlayDataResetEvent(), this);
+		}
+		
+	}
+
+	@Override
+	public void onOverlayDataLoaded(OverlayDataLoadedEvent event) {
+		this.setVisible(true);
+		//get color map here, make grid, and display on legend
+	}
+
 
 }
