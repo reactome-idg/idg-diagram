a6205d2826134c54ecfcbb69c90e03bb28a8cafd
Tim Brunson <tjbrunson@gmail.com>
2019-10-28 13:29:59 -0700

    now displays overlay legend with data types having background fill same as on overlay

--- src/main/java/org/reactome/web/fi/legends/OverlayLegend.java
+++ src/main/java/org/reactome/web/fi/legends/OverlayLegend.java
@@ -1,44 +1,50 @@
 package org.reactome.web.fi.legends;
 
+import java.util.Map;
+
 import org.reactome.web.diagram.common.PwpButton;
 import org.reactome.web.diagram.legends.LegendPanel;
+import org.reactome.web.fi.client.visualisers.diagram.profiles.OverlayColours;
 import org.reactome.web.fi.events.OverlayDataLoadedEvent;
 import org.reactome.web.fi.events.OverlayDataResetEvent;
 import org.reactome.web.fi.handlers.OverlayDataLoadedHandler;
+import org.reactome.web.fi.handlers.OverlayDataResetHandler;
 
+import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.InlineLabel;
 
 public class OverlayLegend extends LegendPanel implements ClickHandler,
-OverlayDataLoadedHandler{
+OverlayDataLoadedHandler, OverlayDataResetHandler{
 	
-	InlineLabel message;
 	private PwpButton closeBtn;
+	private FlowPanel colourMapPanel;
 
 	public OverlayLegend(EventBus eventBus) {
 		super(eventBus);
 		
 		LegendPanelCSS css = RESOURCES.getCSS();
-
-		this.message = new InlineLabel();
-		this.message.setText("Overlay Data Types:");
-		this.add(this.message);
 		
 		this.closeBtn = new PwpButton("Close", css.close(), this);
 		this.add(this.closeBtn);
 		
+		this.colourMapPanel = new FlowPanel();
+		this.add(colourMapPanel);
+		this.getElement().getStyle().setMarginBottom(10, Unit.PX);
+		
 		initHandlers();
 		
 		addStyleName(RESOURCES.getCSS().enrichmentControl());
-		this.setHeight("63px");
 		this.setVisible(false);
 		
 	}
 
 	private void initHandlers() {
 		eventBus.addHandler(OverlayDataLoadedEvent.TYPE, this);
+		eventBus.addHandler(OverlayDataResetEvent.TYPE, this);
 	}
 
 	@Override
@@ -52,9 +58,22 @@ OverlayDataLoadedHandler{
 
 	@Override
 	public void onOverlayDataLoaded(OverlayDataLoadedEvent event) {
+		Map<String, String> map = OverlayColours.get().getColours(event.getEntities().getDataType());
+		map.forEach((k, v) -> {				
+			InlineLabel lbl = new InlineLabel(k);
+			lbl.getElement().getStyle().setBackgroundColor(v);
+			lbl.getElement().getStyle().setPadding(3, Unit.PX);
+			lbl.getElement().getStyle().setMargin(0, Unit.PX);
+			colourMapPanel.add(lbl);
+		});
+		
 		this.setVisible(true);
-		//get color map here, make grid, and display on legend
+
 	}
 
-
+	@Override
+	public void onOverlayDataReset(OverlayDataResetEvent event) {
+		for(int i=0; i<colourMapPanel.getWidgetCount(); i++)
+			colourMapPanel.remove(i);
+	}
 }
