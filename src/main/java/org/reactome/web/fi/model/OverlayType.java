fbebb3b70db08ac389d994b86fc15f563bbf603f
Tim Brunson <tjbrunson@gmail.com>
2019-10-29 13:13:32 -0700

    renamed

---
+++ src/main/java/org/reactome/web/fi/model/OverlayType.java
@@ -0,0 +1,26 @@
+package org.reactome.web.fi.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public enum OverlayType {
+
+	TARGET_DEV_LEVEL;
+	
+	public static OverlayType getType(String type) {
+		if(type == null) return null;
+		for(OverlayType t: values()) {
+			if(t.toString().toLowerCase().equals(type.toLowerCase()))
+				return t;
+		}
+		return null;
+	}
+	
+	public static List<String> getTypes(){
+		List<String> result = new ArrayList<>();
+		for(OverlayType t: values()) {
+			result.add(t.toString().toLowerCase());
+		}
+		return result;
+	}
+}
