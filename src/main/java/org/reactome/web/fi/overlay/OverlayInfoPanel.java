f35fe914b0bfd5560dc6a93f9157c4a7c16c244f
Tim Brunson <tjbrunson@gmail.com>
2019-10-29 13:14:15 -0700

    set up clickListeners for left side buttons

--- src/main/java/org/reactome/web/fi/overlay/OverlayInfoPanel.java
+++ src/main/java/org/reactome/web/fi/overlay/OverlayInfoPanel.java
@@ -1,16 +1,22 @@
 package org.reactome.web.fi.overlay;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.*;
 
 
-public class OverlayInfoPanel extends Composite {
+public class OverlayInfoPanel extends Composite implements ClickHandler{
 	
 	EventBus eventBus;
 	private Button overlayTypes;
 	private Button colours;
+	private List<Button> btns = new LinkedList<>();
 	
 	private DeckLayoutPanel container;
 	
@@ -21,8 +27,10 @@ public class OverlayInfoPanel extends Composite {
 		buttonsPanel.setStyleName(org.reactome.web.diagram.context
 								  .ContextInfoPanel.RESOURCES.getCSS()
 								  .buttonsPanel());
-		buttonsPanel.add(this.overlayTypes = new Button("Overlays"));
-		buttonsPanel.add(this.colours = new Button("Colours"));
+		buttonsPanel.add(this.overlayTypes = new Button("Overlays", this));
+		buttonsPanel.add(this.colours = new Button("Colours", this));
+		btns.add(this.overlayTypes);
+		btns.add(this.colours);
 		
 		this.overlayTypes.addStyleName(org.reactome.web.diagram.context
 									   .ContextInfoPanel.RESOURCES.getCSS()
@@ -44,5 +52,22 @@ public class OverlayInfoPanel extends Composite {
 		outerPanel.add(buttonsPanel);
 		outerPanel.add(this.container);
 		initWidget(outerPanel);
+	}
+
+	@Override
+	public void onClick(ClickEvent event) {
+		for(Button btn: btns) {
+			btn.removeStyleName(org.reactome.web.diagram.context
+									   .ContextInfoPanel.RESOURCES.getCSS()
+									   .buttonSelected());
+		}
+		Button btn = (Button) event.getSource();
+		btn.addStyleName(org.reactome.web.diagram.context
+						 .ContextInfoPanel.RESOURCES.getCSS()
+						 .buttonSelected());
+		if(btn.equals(this.overlayTypes))
+			this.container.showWidget(0);
+		else if(btn.equals(this.colours))
+			this.container.showWidget(1);
 	}
 }
