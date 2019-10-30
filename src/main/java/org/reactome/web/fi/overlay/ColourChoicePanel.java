19d8b8af39fc8217ca11eb91ede935e718eece33
Tim Brunson <tjbrunson@gmail.com>
2019-10-30 11:34:41 -0700

    styled OverlayTypeLabel

--- src/main/java/org/reactome/web/fi/overlay/ColourChoicePanel.java
+++ src/main/java/org/reactome/web/fi/overlay/ColourChoicePanel.java
@@ -6,8 +6,12 @@ import java.util.Map;
 
 import org.reactome.web.fi.client.visualisers.diagram.profiles.OverlayColours;
 
+import com.google.gwt.cell.client.ButtonCellBase.DefaultAppearance.Resources;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.ParagraphElement;
 import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.CssResource;
 
 /**
  * 
@@ -42,6 +46,7 @@ public class ColourChoicePanel extends Composite {
 		overlayColours.forEach((k, v) -> {
 			FlowPanel overlayTypePanel = new FlowPanel();
 			Label overlayTypeLabel = new Label(k);
+			overlayTypeLabel.setStyleName(RESOURCES.getCSS().overlayTypeLabel());
 			overlayTypePanel.add(overlayTypeLabel);
 			v.forEach((i,j) ->{
 				Label colorLbl = new Label(i);
@@ -58,6 +63,24 @@ public class ColourChoicePanel extends Composite {
 		for(int i=1; i<main.getWidgetCount(); i++) 
 			main.remove(i);
 		main.add(coloursScrollPanel);
+	}
+	
+	public static Resources RESOURCES;
+	static {
+		RESOURCES = GWT.create(Resources.class);
+		RESOURCES.getCSS().ensureInjected();
+	}
+	
+	public interface Resources extends ClientBundle {
+		@Source(ResourceCSS.CSS)
+		ResourceCSS getCSS();
+	}
+	
+	@CssResource.ImportedWithPrefix("idgDiagram-colorChoicePanel")
+	public interface ResourceCSS extends CssResource{
+		String CSS = "org/reactome/web/fi/overlay/ColourChoicePanel.css";
+		
+		String overlayTypeLabel();
 	}
 	
 }
