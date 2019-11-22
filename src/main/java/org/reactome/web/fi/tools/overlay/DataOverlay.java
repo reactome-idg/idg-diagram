7eb5eccae806f47e489c3107c0939f21badd8135
Tim Brunson <tjbrunson@gmail.com>
2019-11-22 15:28:00 -0800

    minor change

--- src/main/java/org/reactome/web/fi/tools/overlay/DataOverlay.java
+++ src/main/java/org/reactome/web/fi/tools/overlay/DataOverlay.java
@@ -1,19 +1,32 @@
 package org.reactome.web.fi.tools.overlay;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.InlineLabel;
+import com.google.gwt.user.client.ui.SimplePanel;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.reactome.web.fi.data.loader.TCRDInfoLoader;
 
-public class DataOverlay  extends FlowPanel implements ClickHandler, TCRDInfoLoader.Handler{
+public class DataOverlay  extends FlowPanel implements ClickHandler{
 
 	private List<String> expressionTypes;
 	
 	public DataOverlay() {
+		this.getElement().getStyle().setMargin(5, Unit.PX);
+		
+		SimplePanel title = new SimplePanel();
+		title.add(new InlineLabel("Overlay Data"));
+		title.addStyleName(RESOURCES.getCSS().title());
+		this.add(title);
+		
 		getExpressionTypes();
 		
 	}
@@ -32,23 +45,28 @@ public class DataOverlay  extends FlowPanel implements ClickHandler, TCRDInfoLoa
 			}
 		});
 	}
-	
+
 	@Override
 	public void onClick(ClickEvent event) {
 		// TODO Auto-generated method stub
 		
 	}
-
-	@Override
-	public void onExpressionTypesLoaded(List<String> info) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onTCRDInfoError(Throwable exception) {
-		// TODO Auto-generated method stub
-		
-	}
-
+	
+	public static Resources RESOURCES;
+    static {
+        RESOURCES = GWT.create(Resources.class);
+        RESOURCES.getCSS().ensureInjected();
+    }
+    
+    public interface Resources extends ClientBundle{
+    	@Source(ResourceCSS.CSS)
+    	ResourceCSS getCSS();
+    }
+    
+    @CssResource.ImportedWithPrefix("idg-overlayData")
+    public interface ResourceCSS extends CssResource{
+    	String CSS = "org/reactome/web/fi/tools/overlay/DataOverlay.css";
+    	
+    	String title();
+    }
 }
