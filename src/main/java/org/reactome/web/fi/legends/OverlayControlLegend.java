392899383d163e9d4d219394bb794397b1cfda46
Tim Brunson <tjbrunson@gmail.com>
2019-12-02 08:05:53 -0800

    refactored to require only one class

--- src/main/java/org/reactome/web/fi/legends/OverlayControlLegend.java
+++ src/main/java/org/reactome/web/fi/legends/OverlayControlLegend.java
@@ -14,7 +14,10 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.CssResource;
+import com.google.gwt.resources.client.ImageResource;
+import com.google.gwt.resources.client.ClientBundle.Source;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.InlineLabel;
 import com.google.gwt.user.client.ui.Label;
 
 /**
@@ -26,20 +29,27 @@ public class OverlayControlLegend extends LegendPanel implements ClickHandler, O
 
 	private PwpButton closeBtn;
 	private FlowPanel innerPanel;
+	private PwpButton backButton;
+	private PwpButton forwardButton;
 	
 	public OverlayControlLegend(EventBus eventBus) {
 		super(eventBus);
 		
 		LegendPanelCSS css = RESOURCES.getCSS();
 		
-		this.closeBtn = new PwpButton("Close", css.close(), this);
-		this.add(this.closeBtn);
+		backButton = new PwpButton("Show previous", IDGRESOURCES.getCSS().back(), this);
+		this.add(backButton);
+		forwardButton = new PwpButton("Show next", IDGRESOURCES.getCSS().forward(), this);
+		this.add(forwardButton);
 		
 		this.innerPanel = new FlowPanel();
 		innerPanel.setStyleName(IDGRESOURCES.getCSS().innerPanel());
 		this.add(innerPanel);
 		this.getElement().getStyle().setMarginBottom(10, Unit.PX);
-				
+		
+		this.closeBtn = new PwpButton("Close", css.close(), this);
+		this.add(this.closeBtn);
+		
 		initHandlers();
 		
 		addStyleName(RESOURCES.getCSS().enrichmentControl());
@@ -82,17 +92,30 @@ public class OverlayControlLegend extends LegendPanel implements ClickHandler, O
 	}
 	
 	private void showSingleTissuePanel(OverlayDataLoadedEvent event) {
-		FlowPanel discretePanel = new FlowPanel();
-		discretePanel.add(new Label(event.getDataOverlay().getEType()));
-		innerPanel.add(discretePanel);
+		innerPanel.clear();
+		forwardButton.setEnabled(false);
+		backButton.setEnabled(false);
+		InlineLabel label = new InlineLabel(event.getDataOverlay().getEType());
+		innerPanel.add(label);
 	}
 	
 	private void showMultipleTissuePanel(OverlayDataLoadedEvent event) {
-		ContinuousControlPanel cp = new ContinuousControlPanel(eventBus, event.getDataOverlay());
-		innerPanel.add(cp);
+		innerPanel.clear();
+		forwardButton.setEnabled(true);
+		backButton.setEnabled(true);
+		FlowPanel infoPanel = new FlowPanel();
+		InlineLabel stepLabel = new InlineLabel((event.getDataOverlay().getColumn()+1) + "/" + event.getDataOverlay().getTissueTypes().size() + "  ");
+		infoPanel.add(stepLabel);
+		InlineLabel typeTissueLabel  = new InlineLabel(event.getDataOverlay().getEType() + " - " + event.getDataOverlay().getTissueTypes().get(event.getDataOverlay().getColumn()));
+		infoPanel.add(typeTissueLabel);
+		
+		innerPanel.add(infoPanel);
 	}
 	
 	private void showNoResultsPanel() {
+		innerPanel.clear();
+		forwardButton.setEnabled(false);
+		backButton.setEnabled(false);
 		FlowPanel noResults = new FlowPanel();
 		noResults.add(new Label("No Results. Please try different options!"));
 		innerPanel.add(noResults);
@@ -107,6 +130,30 @@ public class OverlayControlLegend extends LegendPanel implements ClickHandler, O
 	public interface Resources extends ClientBundle{
 		@Source(ResourceCSS.CSS)
 		ResourceCSS getCSS();
+		
+		@Source("images/forward_clicked.png")
+		ImageResource forwardClicked();
+		
+		@Source("images/forward_disabled.png")
+		ImageResource forwardDisabled();
+		
+		@Source("images/forward_hovered.png")
+		ImageResource forwardHovered();
+		
+		@Source("images/forward_normal.png")
+		ImageResource forwardNormal();
+		
+		@Source("images/rewind_clicked.png")
+		ImageResource backClicked();
+		
+		@Source("images/rewind_disabled.png")
+		ImageResource backDisabled();
+		
+		@Source("images/rewind_hovered.png")
+		ImageResource backHovered();
+		
+		@Source("images/rewind_normal.png")
+		ImageResource backNormal();
 	}
 	
 	@CssResource.ImportedWithPrefix("idgDiagram-OverlayControlLegend")
@@ -114,5 +161,9 @@ public class OverlayControlLegend extends LegendPanel implements ClickHandler, O
 		String CSS = "org/reactome/web/fi/legends/OverlayControlLegend.css";
 		
 		String innerPanel();
+		
+		String forward();
+		
+		String back();
 	}
 }
