2ed81a8ddca9423424fc6c326058329d99ec77ea
Tim Brunson <tjbrunson@gmail.com>
2019-12-12 14:48:22 -0800

    removed extra methods and simplified logic

--- src/main/java/org/reactome/web/fi/model/FILayoutType.java
+++ src/main/java/org/reactome/web/fi/model/FILayoutType.java
@@ -25,26 +25,12 @@ public enum FILayoutType {
 		return this.name;
 	}
 	
-	public static FILayoutType lookupType(String type) {
-		String look = type.replace(" ", "_").toUpperCase();
-		return valueOf(look);
-	}
-	
 	public static FILayoutType getType(String type) {
 		if(type==null) return COSE;
 		for(FILayoutType t: values()) {
-			if(t.toString().toLowerCase().equals(type.toLowerCase()))
+			if(t.getName().equals(type))
 				return t;
 		}
 		return COSE;
-	}
-	
-	public static List<String> getLayouts(){
-		List<String> result = new ArrayList<>();
-		for(FILayoutType t: values()) {
-			result.add(t.toString().toLowerCase());
-		}
-		
-		return result;
 	}
 }
