750063e1d503529f362cbab836b12d329cf1ad9c
Tim Brunson <tjbrunson@gmail.com>
2019-12-20 15:08:18 -0800

    updated so filter doesn't interfere with user selection over the scale of the whole unfiltered set of items

--- src/main/java/org/reactome/web/fi/common/MultiSelectListBox.java
+++ src/main/java/org/reactome/web/fi/common/MultiSelectListBox.java
@@ -1,12 +1,13 @@
 package org.reactome.web.fi.common;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import com.google.gwt.event.dom.client.ChangeEvent;
-import com.google.gwt.event.dom.client.ChangeHandler;
-import com.google.gwt.event.shared.HandlerRegistration;
-import com.google.gwt.user.client.ui.ChangeListener;
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.user.client.ui.ListBox;
 
 /**
@@ -17,34 +18,46 @@ import com.google.gwt.user.client.ui.ListBox;
 public class MultiSelectListBox extends ListBox{
 
 	private List<String> totalItems;
-	private List<String> selectedItems;
+	private Set<String> selectedItems;
 	
 	public MultiSelectListBox() {
+		selectedItems = new HashSet<>();
+		this.addChangeHandler(e -> onChange(e));
 	}	
 
-	public List<String> getSelectedItemsText(){
-		List<String> result = new ArrayList<>();
+	/**
+	 * Manipulates selectedItems variable every time an item is selected or de-selected.
+	 * This means that even with the filter function manipulating the items in the list box, 
+	 * user selected items stay selected.
+	 * 
+	 * @param e
+	 */
+	private void onChange(ChangeEvent e) {
+		List<String> add = new ArrayList<>();
 		for(int i=0; i<getItemCount(); i++) {
 			if(isItemSelected(i))
-				result.add(this.getItemText(i));
+				add.add(this.getItemText(i));
 		}
-		return result;
+		selectedItems.addAll(add);
+		
+		List<String> remove = new ArrayList<>();
+		for(int i=0; i<getItemCount(); i++) {
+			if(!isItemSelected(i))
+				remove.add(this.getItemText(i));
+		}
+		selectedItems.removeAll(remove);
 	}
 
-	public List<Integer> getSelectedItemsIndexs(){
-		List<Integer> result = new ArrayList<>();
-		for(int i=0; i<getItemCount(); i++) {
-			if(isItemSelected(i))
-				result.add(i);
-		}
-		return result;
+	public List<String> getSelectedItemsText(){
+		return selectedItems.stream().collect(Collectors.toList());
 	}
 	
 	public void filter(String filter) {
 		if(totalItems == null) return;
+		
 		this.clear();
 		for(String item : totalItems)
-			if(item.contains(filter))
+			if(item.toLowerCase().contains(filter.toLowerCase()))
 				this.addItem(item);
 		
 		resetSelected();
@@ -63,6 +76,4 @@ public class MultiSelectListBox extends ListBox{
 		for(String item : items)
 			this.addItem(item);
 	}
-	
-	
 }
