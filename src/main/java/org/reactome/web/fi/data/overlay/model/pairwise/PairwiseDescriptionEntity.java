e2d0c714db43d2d0e6c8838c9d4e311ba9b35dc3
Tim Brunson <tjbrunson@gmail.com>
2020-01-07 18:35:21 -0800

    created autobean interfaces for pairwiseDescription

---
+++ src/main/java/org/reactome/web/fi/data/overlay/model/pairwise/PairwiseDescriptionEntity.java
@@ -0,0 +1,17 @@
+package org.reactome.web.fi.data.overlay.model.pairwise;
+
+/**
+ * 
+ * @author brunsont
+ *
+ */
+public interface PairwiseDescriptionEntity {
+
+	String getId();
+	
+	String getProvenance();
+	
+	String getDataType();
+	
+	String getBioSource();
+}
