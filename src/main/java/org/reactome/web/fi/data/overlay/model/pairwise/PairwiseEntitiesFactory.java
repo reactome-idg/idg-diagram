a9cf18b727f29c8efd3c22b37d0b73059d6cdc8a
Tim Brunson <tjbrunson@gmail.com>
2020-01-09 15:29:42 -0800

    added autobean factory for pairwise data

---
+++ src/main/java/org/reactome/web/fi/data/overlay/model/pairwise/PairwiseEntitiesFactory.java
@@ -0,0 +1,31 @@
+package org.reactome.web.fi.data.overlay.model.pairwise;
+
+import com.google.gwt.core.client.GWT;
+import com.google.web.bindery.autobean.shared.AutoBean;
+import com.google.web.bindery.autobean.shared.AutoBeanCodex;
+import com.google.web.bindery.autobean.shared.AutoBeanFactory;
+
+/**
+ * 
+ * @author brunsont
+ *
+ */
+public class PairwiseEntitiesFactory {
+
+	interface ModelAutoBeanFactory extends AutoBeanFactory{
+		AutoBean<PairwiseEntities> pairwiseEntities();
+		AutoBean<PairwiseEntity> pairwiseEntity();
+		AutoBean<DataDescription> dataDescription();
+	}
+	
+	public static <T> T getPairwiseEntities(Class<T> cls, String json) throws Exception{
+		try {
+			AutoBeanFactory factory = GWT.create(ModelAutoBeanFactory.class);
+			AutoBean<T> bean = AutoBeanCodex.decode(factory, cls, json);
+			return bean.as();
+		}catch(Throwable e) {
+			throw new Exception("AutoBean could not be generated for pairwise entities.");
+		}
+	}
+	
+}
