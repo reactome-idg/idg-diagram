e1d0771c2ab7fad08fe72e3bbb0861a35ed620d6
Tim Brunson <tjbrunson@gmail.com>
2020-01-10 09:40:21 -0800

    initial setup for Pairwise data loader

--- src/main/java/org/reactome/web/fi/data/loader/PairwiseDataLoader.java
+++ src/main/java/org/reactome/web/fi/data/loader/PairwiseDataLoader.java
@@ -3,14 +3,35 @@ package org.reactome.web.fi.data.loader;
 import com.google.gwt.http.client.Request;
 import com.google.gwt.http.client.RequestCallback;
 import com.google.gwt.http.client.Response;
+import org.reactome.web.fi.data.overlay.model.pairwise.PairwiseOverlayProperties;
 
 public class PairwiseDataLoader implements RequestCallback{
 
 	public interface Handler{
 		void onPairwiseDataLoaded();
-		void onPairwiseDataLoadedError();
+		void onPairwiseDataLoadedError(Exception exception);
+	}
+	
+	private final static String BASE_URL = "/idgpairwise";
+	
+	private Handler handler;
+	private Request request;
+	private PairwiseOverlayProperties properties;
+
+	public PairwiseDataLoader(Handler handler) {
+		this.handler = handler;
 	}
 
+	public void load(PairwiseOverlayProperties properties) {
+		this.properties = properties;
+		cancel();
+		
+		if(properties.getUniprots() == null) {
+			Exception exception = new Exception("Cannot request overlay data for 0 ids.");
+			this.handler.onPairwiseDataLoadedError(exception);
+		}
+	}
+	
 	@Override
 	public void onResponseReceived(Request request, Response response) {
 		// TODO Auto-generated method stub
@@ -21,6 +42,11 @@ public class PairwiseDataLoader implements RequestCallback{
 	public void onError(Request request, Throwable exception) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	public void cancel() {
+		if(this.request != null && this.request.isPending())
+			this.request.cancel();
 	}
 	
 }
