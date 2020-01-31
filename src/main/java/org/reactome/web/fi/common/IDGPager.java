1c3bf4a9b0b77451e81f366a262141fd63b156a1
Tim Brunson <tjbrunson@gmail.com>
2020-01-31 12:39:45 -0800

    added extension for pager

---
+++ src/main/java/org/reactome/web/fi/common/IDGPager.java
@@ -0,0 +1,31 @@
+package org.reactome.web.fi.common;
+
+import com.google.gwt.user.cellview.client.SimplePager;
+
+public class IDGPager extends SimplePager{
+
+	@Override
+	public void previousPage() {
+		// TODO Auto-generated method stub
+		super.previousPage();
+	}
+
+	@Override
+	public void nextPage() {
+		// TODO Auto-generated method stub
+		super.nextPage();
+	}
+
+	@Override
+	public void lastPage() {
+		// TODO Auto-generated method stub
+		super.lastPage();
+	}
+
+	@Override
+	public void firstPage() {
+		// TODO Auto-generated method stub
+		super.firstPage();
+	}
+	
+}
