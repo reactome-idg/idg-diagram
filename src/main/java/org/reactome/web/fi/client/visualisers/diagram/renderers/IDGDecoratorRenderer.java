de8e672e25b6ec644314ac01e069dfae740b1f6c
Tim Brunson <tjbrunson@gmail.com>
2020-02-24 10:21:43 -0800

    prototyping adding correct number of interactors to complex and entity set decorators

--- src/main/java/org/reactome/web/fi/client/visualisers/diagram/renderers/IDGDecoratorRenderer.java
+++ src/main/java/org/reactome/web/fi/client/visualisers/diagram/renderers/IDGDecoratorRenderer.java
@@ -1,5 +1,8 @@
 package org.reactome.web.fi.client.visualisers.diagram.renderers;
 
+import java.util.Set;
+
+import org.reactome.web.diagram.data.graph.model.GraphPhysicalEntity;
 import org.reactome.web.diagram.data.layout.Coordinate;
 import org.reactome.web.diagram.data.layout.DiagramObject;
 import org.reactome.web.diagram.data.layout.Node;
@@ -11,12 +14,22 @@ import org.reactome.web.diagram.util.AdvancedContext2d;
 import org.reactome.web.fi.data.layout.ShapeImpl;
 import org.reactome.web.fi.data.layout.SummaryItemImpl;
 
+/**
+ * 
+ * @author brunsont
+ *
+ */
 public class IDGDecoratorRenderer {
 
-	public IDGDecoratorRenderer() {
-		
-	}
+	public IDGDecoratorRenderer() {/* Nothing Here */}
 	
+	/**
+	 * performs render of decorator on a passed in diagramObject in the diagram view.
+	 * @param ctx
+	 * @param obj
+	 * @param factor
+	 * @param offset
+	 */
 	public void doRender(AdvancedContext2d ctx, DiagramObject obj, Double factor, Coordinate offset) {
 		SummaryItem summaryItem = makeSummaryItem(obj);
 		//dont render if no interactions exist
@@ -24,17 +37,32 @@ public class IDGDecoratorRenderer {
 		SummaryItemAbstractRenderer.draw(ctx, summaryItem, factor, offset);
 	}
 
+	/**
+	 * Gets summary item needed to render decorator
+	 * @param obj
+	 * @return
+	 */
 	private SummaryItem makeSummaryItem(DiagramObject obj) {
 		SummaryItem result = new SummaryItemImpl(getShape(obj), getNumber(obj));
 		
 		return result;
 	}
 
+	/**
+	 * Makes shape for decorator. Always returns a circle with a radius of 6px.
+	 * @param obj
+	 * @return
+	 */
 	private Shape getShape(DiagramObject obj) {
 		Shape result = new ShapeImpl(getCoordinate(obj), new Double(6), "CIRCLE", true);
 		return result;
 	}
 	
+	/**
+	 * Gets coordinate for decorator
+	 * @param obj
+	 * @return
+	 */
 	private Coordinate getCoordinate(DiagramObject obj) {
 		Node node = (Node) obj;
 		double x = node.getProp().getX() + node.getProp().getWidth();
@@ -42,9 +70,25 @@ public class IDGDecoratorRenderer {
 		Coordinate result = CoordinateFactory.get(x, y);
 		return result;
 	}
-
+	
+	/**
+	 * Get number of interactors for all protein participants of a given complex or Entity Set
+	 */
 	private Integer getNumber(DiagramObject obj) {
-		return 100; //test value for now
+		int result = 0;
+		
+		GraphPhysicalEntity entity = obj.getGraphObject(); //should always be GraphPhysicalEntity
+		Set<GraphPhysicalEntity> peSet = entity.getParticipants();
+		for(GraphPhysicalEntity pe : peSet) {
+			for(DiagramObject diagramObject : pe.getDiagramObjects()) {
+				Node node = (Node)diagramObject;
+				if(node.getDiagramEntityInteractorsSummary() != null) {
+					result += node.getDiagramEntityInteractorsSummary().getNumber();
+				}
+			}
+		}
+		
+		return result;
 	}
 	
 }
