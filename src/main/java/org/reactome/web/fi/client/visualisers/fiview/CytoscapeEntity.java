d97ccc1a8bf095835a2a341fb4768c181183125b
Tim Brunson <tjbrunson@gmail.com>
2020-02-24 14:31:46 -0800

    now tracks current layout

--- src/main/java/org/reactome/web/fi/client/visualisers/fiview/CytoscapeEntity.java
+++ src/main/java/org/reactome/web/fi/client/visualisers/fiview/CytoscapeEntity.java
@@ -12,9 +12,21 @@ import com.google.gwt.event.shared.EventBus;
 public class CytoscapeEntity extends CytoscapeWrapper{
 
 	String baseStyle;
+	String layout;
 	
 	public CytoscapeEntity(String baseStyle, Handler handler) {
 		super(baseStyle, handler);
 		this.baseStyle = baseStyle;
+	}
+
+	@Override
+	public void setCytoscapeLayout(String layoutString) {
+		this.layout = layoutString;
+		super.setCytoscapeLayout(layoutString);
 	}	
+	
+	public String getLayout() {
+		return layout;
+	}
+	
 }
