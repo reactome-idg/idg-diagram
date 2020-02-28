a2f55aa759569f21eef0a53aa80c8e3feff03388
Tim Brunson <tjbrunson@gmail.com>
2020-02-28 09:53:05 -0800

    added hover tooltip to table

--- src/main/java/org/reactome/web/fi/tools/overlay/pairwise/results/PairwisePopupResultsTable.java
+++ src/main/java/org/reactome/web/fi/tools/overlay/pairwise/results/PairwisePopupResultsTable.java
@@ -10,7 +10,9 @@ import org.reactome.web.fi.tools.overlay.pairwise.results.columns.PairwiseIntera
 import org.reactome.web.fi.tools.overlay.pairwise.results.columns.PairwiseRelationshipColumn;
 import org.reactome.web.fi.tools.overlay.pairwise.results.columns.PairwiseSourceColumn;
 
+import com.google.gwt.dom.client.Element;
 import com.google.gwt.user.cellview.client.DataGrid;
+import com.google.gwt.view.client.CellPreviewEvent;
 import com.google.gwt.view.client.ListDataProvider;
 
 /**
@@ -33,11 +35,22 @@ public class PairwisePopupResultsTable extends DataGrid<PairwiseTableEntity>{
 		
 
 		this.addColumn(new DiagramGeneNameColumn(), "Diagram Source");
-		PairwiseInteractorColumn pairwiseColumn;
-		this.addColumn(pairwiseColumn = new PairwiseInteractorColumn(), "Pairwise Interactor");
+		this.addColumn(new PairwiseInteractorColumn(), "Pairwise Interactor");
 		this.addColumn(new OverlayValueColumn(), "Overlay Value");
 		this.addColumn(new PairwiseRelationshipColumn(), "Pos/Neg");
 		this.addColumn(new PairwiseSourceColumn(), "Interaction Source");
+		
+		this.addCellPreviewHandler(new CellPreviewEvent.Handler<PairwiseTableEntity>() {
+
+            @Override
+            public void onCellPreview(final CellPreviewEvent<PairwiseTableEntity> event) {
+                if (!event.getNativeEvent().getType().equals("mouseover")) return;
+                Element cellElement = event.getNativeEvent().getEventTarget().cast();
+                PairwiseTableEntity model = (PairwiseTableEntity) PairwisePopupResultsTable.this.getVisibleItem(event.getIndex());
+                cellElement.setTitle(PairwisePopupResultsTable.this.getColumn(event.getColumn()).getValue(model)+"");
+                
+            }
+        });
 				
 		//ListDataProvider setup
 		provider.addDataDisplay(this);
