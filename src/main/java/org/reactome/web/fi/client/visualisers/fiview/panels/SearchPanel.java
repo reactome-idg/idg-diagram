f453b14c07148403e03601012921529238c78e06
Tim Brunson <tjbrunson@gmail.com>
2020-04-27 09:25:09 -0700

    initial setup for FI search panel

--- src/main/java/org/reactome/web/fi/client/visualisers/fiview/panels/SearchPanel.java
+++ src/main/java/org/reactome/web/fi/client/visualisers/fiview/panels/SearchPanel.java
@@ -1,5 +1,9 @@
 package org.reactome.web.fi.client.visualisers.fiview.panels;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.reactome.web.diagram.common.IconButton;
 
 import com.google.gwt.core.client.GWT;
@@ -7,8 +11,8 @@ import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.resources.client.ImageResource;
-import com.google.gwt.resources.client.ClientBundle.Source;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.TextBox;
 
 /**
  * 
@@ -17,8 +21,13 @@ import com.google.gwt.user.client.ui.FlowPanel;
  */
 public class SearchPanel extends FlowPanel{
 
+	private final String OPENING_TEXT = "Search FI nodes";
+	private final int SEARCH_QUERY_MINIMUM_LENGTH = 5;
+	
 	private EventBus eventBus;
 	private boolean isExpanded = false;
+	
+	private TextBox input;
 	
 	public SearchPanel(EventBus eventBus) {
 		setStyleName(RESOURCES.getCSS().launchPanel());
@@ -30,29 +39,59 @@ public class SearchPanel extends FlowPanel{
 	
 	private void initPanel() {
 		IconButton searchBtn = new IconButton(RESOURCES.searchIcon(), RESOURCES.getCSS().launch(),"Search Nodes", e -> expandSearch());
-	
+		this.add(searchBtn);
+		
+		this.input = new TextBox();
+		this.input.setStyleName(RESOURCES.getCSS().input());
+		this.input.getElement().setPropertyString("placeholder", OPENING_TEXT);
+		this.input.getElement().setPropertyBoolean("spellcheck", false);
+		this.add(input);
+		
+		IconButton clearBtn = new IconButton("", RESOURCES.clear());
+		clearBtn.setStyleName(RESOURCES.getCSS().clearBtn());
+		clearBtn.setVisible(false);
+		clearBtn.setTitle("Clear search");
+		clearBtn.addClickHandler(e -> onClearButtonClicked());
+		this.add(clearBtn);
+		
+		IconButton executeBtn = new IconButton("", RESOURCES.searchGo());
+		executeBtn.setStyleName(RESOURCES.getCSS().executeBtn());
+		executeBtn.setVisible(true);
+		executeBtn.setTitle("Search");
+		executeBtn.addClickHandler(e -> executeBtnClicked());
+		this.add(executeBtn);
+		
+	}
+	private void executeBtnClicked() {
+		String query = input.getText().trim();
+		if(query.length() < SEARCH_QUERY_MINIMUM_LENGTH) return;
+		
+		Set<String> queryItems = new HashSet<>(Arrays.asList(query.split(",")));
+		//TODO: fire event to search FIView for queryItems
 	}
 
+
+	private void onClearButtonClicked() {
+		if(!input.getValue().isEmpty()) {
+			input.setValue("");
+			setFocus(true);
+		}
+	}
+
+	private void setFocus(boolean focused) {
+		this.input.setFocus(focused);
+	}
 
 	private void expandSearch() {
 		if(!isExpanded)
 			expandPanel();
 		else {
 			collapsePanel();
-			stopExpandingByDefault();
 		}
 	}
 
-
-	private void stopExpandingByDefault() {
-		// TODO Auto-generated method stub
-		
-	}
-
-
 	private void collapsePanel() {
-		// TODO Auto-generated method stub
-		
+		removeStyleName(RESOURCES.getCSS().launchPanelExpanded());
 	}
 
 
@@ -89,6 +128,14 @@ public class SearchPanel extends FlowPanel{
 		String launchPanel();
 		
 		String launch();
+		
+		String input();
+		
+		String clearBtn();
+		
+		String executeBtn();
+		
+		String launchPanelExpanded();
 	}
 	
 }
